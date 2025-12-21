8f4ec06f0d2c5378413a53616f4c8b62790f030c
tcvdh <tcvdh@outlook.com>
2025-12-21 01:00:15 +0100

    back to gcc again..

--- bootloader/main.c
+++ bootloader/main.c
@@ -1,13 +1,16 @@
 #include <efi.h>
 #include <efilib.h>
 
-EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *SystemTable) {
-    SystemTable->ConOut->Reset(SystemTable->ConOut, FALSE);
-    SystemTable->ConOut->SetAttribute(SystemTable->ConOut, EFI_TEXT_ATTR(EFI_WHITE, EFI_BLUE));
-    SystemTable->ConOut->ClearScreen(SystemTable->ConOut);
-    SystemTable->ConOut->OutputString(SystemTable->ConOut, L"Hello, UEFI World!\r\n\r\n");
-    while(1) {
-        __asm__ __volatile__ ("hlt");
+EFI_STATUS EFIAPI efi_main(EFI_HANDLE ImageHandle, EFI_SYSTEM_TABLE *ST) {
+    uefi_call_wrapper(ST->ConOut->Reset, 2, ST->ConOut, FALSE);
+    uefi_call_wrapper(ST->ConOut->ClearScreen, 1, ST->ConOut);
+    uefi_call_wrapper(ST->ConOut->SetAttribute, 2, ST->ConOut, EFI_TEXT_ATTR(EFI_LIGHTGRAY, EFI_BLACK));
+
+    InitializeLib(ImageHandle, ST);
+    Print(L"Hello, world!\n");
+
+    while (1) {
+        __asm__ __volatile__("hlt");
     }
 
     return EFI_SUCCESS;
